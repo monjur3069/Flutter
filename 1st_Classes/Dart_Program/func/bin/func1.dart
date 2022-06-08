0e4d8bb02800f1011d110d33a3b1eac195fe9ff1
Monjur Ahmed Akash <44949690+monjur3069@users.noreply.github.com>
2022-06-09 03:16:48 +0600

    Delete 1st_Classes directory

--- 1st_Classes/Dart_Program/func/bin/func1.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  getA(2.36);
-}
-
-dynamic getA(double r, [String pi = ''])
-{
-    print(r);
-}
--- 1st_Classes/Dart_Program/list_map/test/list_map_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:list_map/list_map.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
