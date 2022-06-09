9d9b4e6a02b747098de2eac7139e5b509cfcd7d8
Monjur Ahmed Akash <44949690+monjur3069@users.noreply.github.com>
2022-06-09 23:19:41 +0600

    Delete 3rd_Class/class2 directory

--- 3rd_Class/class2/bin/models/Features/features.dart
+++
@@ -1,8 +0,0 @@
-class Fly{
-  String canFly() => 'Can Fly';
-}
-
-class Transform {
-  String canTransform() => 'Can Transform';
-}
-
--- 3rd_Class/class2/test/class2_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:class2/class2.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
